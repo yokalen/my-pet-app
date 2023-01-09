fd1efc3498865d7a80801a76d38e4f61201143dd
My Computer <yokalen@gmail.com>
2023-01-09 13:52:37 -0800

    fix logout error

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,15 +44,20 @@ exports.postLogin = (req, res, next) => {
   })(req, res, next);
 };
 
-exports.logout = (req, res) => {
-  req.logout(() => {
-    console.log('User has logged out.')
-  })
-  req.session.destroy((err) => {
-    if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
-    req.user = null;
-    res.redirect("/");
+exports.logout = (req, res, next) => {
+
+  req.logout((err) => {
+    if(err) {
+      return next(err);
+    }
+    console.log('User has logged out.');
+    //moved req.session.destroy inside req.logout callback to fix err
+    req.session.destroy((err) => {
+      if (err)
+        console.log("Error : Failed to destroy the session during logout.", err);
+      req.user = null;
+      res.redirect("/");
+    });
   });
 };
 
