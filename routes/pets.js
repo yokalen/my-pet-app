b1be013ee7c3a54e1f5391eff387ebffce975b80
My Computer <yokalen@gmail.com>
2023-01-11 16:42:30 -0800

    update

--- routes/pets.js
+++ routes/pets.js
@@ -4,12 +4,9 @@ const upload = require("../middleware/multer");
 const petsController = require("../controllers/pets");
 const { ensureAuth } = require("../middleware/auth");
 
-//Post Routes - simplified for now
 router.get("/:id", ensureAuth, petsController.getPet);
 
 router.post("/createPet", upload.single("file"), petsController.createPet);
-
-// router.put("/likePost/:id", postsController.likePost);
 
 router.delete("/deletePet/:id", petsController.deletePet);
 
