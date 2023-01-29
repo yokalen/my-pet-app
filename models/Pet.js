86423c4350eb4e4c4775d1081fd2b06f029f5703
My Computer <yokalen@gmail.com>
2023-01-29 15:04:12 -0800

    Add size property that is calculated before saving a pet based on type of animal and weight

--- models/Pet.js
+++ models/Pet.js
@@ -1,4 +1,31 @@
 const mongoose = require("mongoose");
+const SIZE_RULES = {
+  // defines the size rules for each animal type 
+  cat: [
+    { weight: 0, size: 'small' },
+    { weight: 10, size: 'medium' },
+    { weight: 20, size: 'large' },
+  ],
+  dog: [
+    { weight: 0, size: 'small' },
+    { weight: 45, size: 'medium' },
+    { weight: 75, size: 'large' },
+    { weight: Infinity, size: 'extra-large' },
+  ],
+  // Add additional animal types and size rules as needed
+};
+
+function getSize(animal, weight){
+  //sets the size property based on type of animal and their weight
+  const rules = SIZE_RULES[animal.toLowerCase()];
+  for(const { weight: maxWeight, size } of rules) {
+    if(weight <= maxWeight){
+      return size;
+    }
+  }
+  //when size unknown leave an empty string
+  return '';
+}
 
 const PetSchema = new mongoose.Schema({
   animal: {
@@ -29,6 +56,11 @@ const PetSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  size: {
+    type: String,
+    required: true,
+    default: '',
+  },
   dob: {
     type: Number,
     required: true,
@@ -41,6 +73,11 @@ const PetSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+PetSchema.pre('save', function(){
+  //runs before saving a 'Pet' to the database, sets 'size' property
+  this.size = getSize(this.animal, this.weight);
 });
 
 module.exports = mongoose.model("Pet", PetSchema);
